c8a2aa04fa156b32e9026849ad1457254bb1c28f
OverkillU <micha.vonbank@gmail.com>
2016-08-14 23:03:56 +0200

    DAYUM SUCH SEXY
    init commit

---
+++ Irgendwie/Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public GameObject Cube;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("d"))
+			Cube.transform.Translate(Vector2.right);
+
+		if (Input.GetKey ("a"))
+			Cube.transform.Translate(Vector2.left);
+	
+		if (Input.GetKey ("w"))
+			Cube.transform.Translate(Vector2.up);
+
+		if (Input.GetKey ("s"))
+			Cube.transform.Translate(Vector2.down);
+	}
+}
